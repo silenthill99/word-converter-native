8322660f7ab038ac4a403e72ccc3e645e85b172c
silenthill99 <florian.graziani@sfr.fr>
2025-09-16 15:03:55 +0200

    Mise à jour du projet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <TextInput style={styles.form}/>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    form: {
+        height: 25,
+        width: '25%',
+        backgroundColor: "white",
+        borderWidth: 2,
+    }
+})
